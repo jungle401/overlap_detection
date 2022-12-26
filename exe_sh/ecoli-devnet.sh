6902f3e87219beb5d807d517a9c391fe0f7e90b9
jungle401 <johnson401.cs09@nycu.edu.tw>
2022-12-26 17:20:24 +0800

    high FP

--- exe_sh/ecoli-devnet.sh
+++ exe_sh/ecoli-devnet.sh
@@ -1,6 +1,6 @@
 ./app \
 --reads_fasta /mnt/es/ness/johnson/thesis/data/Ecoli/reads/aligned_and_sampled/minimap2/DevNet-P6C4/rngAll/aln.lsmq10.fasta \
---fmi_src load \
+--fmi_src build \
 --fmi_dir ../mid_files/devnet \
 --kmer_size 16 \
 --dist_hori_filter 800 \
@@ -13,4 +13,4 @@
 --min_antiDiag_space 80 \
 --batchSize_readsNum 70000 \
 --output_dir ../output/devnet
-~/thesis/evaluation/src/exe_shs/minOlen_2000/ecoli/rngAll/ecoli-devNet_mime.sh
+# ~/thesis/evaluation/src/exe_shs/minOlen_2000/ecoli/rngAll/ecoli-devNet_mime.sh
